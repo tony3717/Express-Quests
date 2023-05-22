32c33fca2127dddf9e19be58a3b681cfc6959450
ANTHONY GENTY <gentytony@gmail.com>
2023-05-22 17:10:57 +0200

    init du proj

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
 
-const port = 5000;
+app.use(express.json());
+
+const port = process.env.APP_PORT ?? 5000;
+
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
 };
+
 
 app.get("/", welcome);
 
@@ -14,6 +20,14 @@ const movieHandlers = require("./movieHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.post("/api/movies", movieHandlers.postMovie);
+
+const userHandlers = require("./userHandlers");
+
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+app.post("/api/users", userHandlers.postUser);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
 
 app.listen(port, (err) => {
   if (err) {
